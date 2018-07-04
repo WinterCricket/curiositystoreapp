95bc26702da5273e69b2ecbb1aa5884321f34a3f
WinterCricket <hodhodhod2003@gmail.com>
2018-07-04 00:55:48 -0500

    add view file

---
+++ curiousview.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <style>
+                .tableColor {
+                        background-color: black;
+                        color: white;
+                }
+        </style>
+
+        <title >View Records</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+<body>
+        
+        <h1>View Records</h1>
+        
+        <p><b>View All</b> | <a href="yellview-paginated.php">View Paginated</a></p>
+        
+        <?php
+                        // connect to the database
+        include('yellconnect-db.php');
+        
+                        // get the records from the database
+        if ($result = $mysqli->query("SELECT * FROM products ORDER BY id"))
+        {
+                                // display records if there are records to display
+                if ($result->num_rows > 0)
+                {
+                                        // display records in a table
+                        echo "<table border='1' cellpadding='10'>";
+                        
+                                        // set table headers
+                        echo "<tr> <th>ID</th> <th>Name</th> <th>Description</th> <th>Price</th> <th></th> <th></th> </tr>";
+                        
+                        while ($row = $result->fetch_object())
+                        {
+                                                // set up a row for each record
+                                echo "<tr>";
+                                echo "<td >" . $row->id . "</td>";
+                                echo "<td>" . $row->name . "</td>";
+                                echo "<td >" . $row->description . "</td>";
+                                echo "<td class='tableColor'>" . $row->price . "</td>";
+                                echo "<td><a href='yellrecords.php?id=" . $row->id . "'>Edit</a></td>";
+                                echo "<td><a href='yelldelete.php?id=" . $row->id . "'>Delete</a></td>";
+                                echo "</tr>";
+                        }
+                        
+                        echo "</table>";
+                }
+                                // if there are no records in the database, display an alert message
+                else
+                {
+                        echo "No results to display!";
+                }
+        }
+                        // show an error if there is an issue with the database query
+        else
+        {
+                echo "Error: " . $mysqli->error;
+        }
+        
+                        // close database connection
+        $mysqli->close();
+        
+        ?>
+        
+        <a href="yellrecords.php">Add New Record</a>
+</body>
+</html>
